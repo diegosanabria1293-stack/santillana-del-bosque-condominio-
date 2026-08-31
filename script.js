5eb7a07a9a2c2df4bb08c2f074e7694c533bcb60
diegosanabria1293-stack <diegosanabria1293@gmail.com>
2026-08-31 08:33:36 -0500

    Conectar el índice con la landing mejorada

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Santilana del Bosque");
+(() => {
+  const oldStylesheet = document.querySelector('link[href="style.css"]');
+  if (oldStylesheet) oldStylesheet.remove();
+  document.body.innerHTML = '<div id="root"></div>';
+  const stylesheet = document.createElement('link');
+  stylesheet.rel = 'stylesheet';
+  stylesheet.href = '/assets/index-BjwF_rMp.css';
+  document.head.appendChild(stylesheet);
+  const app = document.createElement('script');
+  app.type = 'module';
+  app.src = '/assets/index-bQlaO-c9.js';
+  document.head.appendChild(app);
+})();
